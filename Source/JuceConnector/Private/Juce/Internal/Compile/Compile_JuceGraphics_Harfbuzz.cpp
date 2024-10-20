242760a11427dab77990ab661c9f1ac57eec4b91
nErumin <nerumin90@gmail.com>
2024-10-21 01:00:53 +0900

    [Build] Restore Unreal macros when compiling JUCE.

--- Source/JuceConnector/Private/Juce/Internal/Compile/Compile_JuceGraphics_Harfbuzz.cpp
+++ Source/JuceConnector/Private/Juce/Internal/Compile/Compile_JuceGraphics_Harfbuzz.cpp
@@ -7,3 +7,12 @@
 JUCE_INCLUDES_START
 #include "juce_graphics/juce_graphics_Harfbuzz.cpp"
 JUCE_INCLUDES_END
+
+/* Restore the Unreal macro definitions. Their definition are from Misc/AssertionMacros.h. */
+#if DO_ENSURE && !USING_CODE_ANALYSIS
+#define ensure(InExpression) UE_ENSURE_IMPL (false, InExpression)
+#endif
+
+#if DO_CHECK
+#define verify(expr) UE_CHECK_IMPL(expr)
+#endif
